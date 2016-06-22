aa3edbb59006313b51aa7f03c1e2a381001164e0
Dirceu Vieira <dirceuvjr@gmail.com>
2016-06-22 19:14:06 -0300

    tooltip issue  try to make tooltip work properly on heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
     $(e.trigger).tooltip('show');
     setTimeout(
       function () {
-        $(e.trigger).tooltip('hide');
+        $(e.trigger).tooltip('dispose');
       },
       1000
     );
